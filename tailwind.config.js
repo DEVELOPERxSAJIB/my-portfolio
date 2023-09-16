aca414f4bf83722e642f8cc0ec01c7394c6cea60
DEVELOPERxSAJIB <sajibshikder78971@gmail.com>
2023-09-17 03:18:16 +0600

    unnecessary file deletes

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/hero3.jpg')",
+        // "hero-pattern": "url('/src/assets/hero3.jpg')",
       },
     },
   },
